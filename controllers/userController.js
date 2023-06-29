3fadd96d08d3048adfe453f68fd8b61ffe199864
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-06-29 23:06:59 +0530

    Update useController
    implemented the functionalities of get-users route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,20 @@
 const userModel = require('../models/userModel')
 
 
-//get all users || GET Request
-exports.getAllUsers = () => {
-
-}
-
-//create registered user || POST Request
+// 1. CREATE REGISTERED USER || POST REQUEST
 exports.registerController = async (req, res) => {
     try {
       const { username, email, password } = req.body;
-      //validation
+      
+      //VALIDATION
       if (!username || !email || !password) {
         return res.status(400).send({
           success: false,
           message: "Please Fill all fields",
         });
       }
-      //exisiting user
+
+      //CHECKING IF USER ALREADY EXISTS
       const exisitingUser = await userModel.findOne({ email });
       if (exisitingUser) {
         return res.status(401).send({
@@ -26,7 +23,7 @@ exports.registerController = async (req, res) => {
         });
       }
   
-      //save new user
+      //SAVE NEW USER
       const user = new userModel({ username, email, password});
       await user.save();
       return res.status(201).send({
@@ -45,7 +42,27 @@ exports.registerController = async (req, res) => {
     }
   };
 
-//Login Controller || POST Request
+// 2. GET ALL USERS || GET Request
+exports.getAllUsers = async (req,res) => {
+  try{
+   const users = await userModel.find({});
+   return res.status(200).send({
+    success: "true",
+    userCount: users.length,
+    message: "responded with all users data",
+    users
+   })
+  }
+  catch(error){
+   return res.status(500).send({
+    success: false,
+    message: "Error in getting all data",
+    error
+   })
+  }
+}
+
+// 3. LOGIN CONTROLLER || POST REQUEST
 exports.loginController = () => {
 
 }

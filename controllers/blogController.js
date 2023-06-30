7289f03fccd9baf9365f88beaebc876a877f1496
techcursed <31003460+TechCursed@users.noreply.github.com>
2023-06-30 18:18:40 +0530

    implemented createBlogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -29,7 +29,31 @@ exports.getAllBlogsController = async (req,res) => {
 }
 
 //POST || CREATE BLOG
-exports.createBlogController = () => {
+exports.createBlogController = async (req,res) => {
+    try{
+      const {title, description, image} = req.body;
+      //VALIDATION
+      if(!title || !description || !image){
+        return res.status(400).send({
+            success: false,
+            message: "Please provide all fields"
+        })
+      }
+      const newBlog = new blogModel({title, description, image});
+      await newBlog.save();
+      return res.status(201).send({
+          success: true,
+          message: "Blog created"
+      })
+    }
+    catch(error){
+      console.log(error)
+      return res.status(400).send({
+        success: false,
+        message: "Error while creating blog",
+        error
+      })
+    }
 
 }
 
